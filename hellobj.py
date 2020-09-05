99f38123fd28d9beaf46d767bb5c2e7b4751d8e0
Noah Gift <noah.gift@gmail.com>
2020-09-05 01:47:07 +0000

    adding working computer vision project

--- hellobj.py
+++ hellobj.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
-# import boto3
+import boto3
 
 
 def detect_labels(bucket, image):
     print(f"This is the bucket {bucket}, and this is the image {image}")
+    rekognition = boto3.client("rekognition")
+    response = rekognition.detect_text(
+        Image={
+            "S3Object": {
+                "Bucket": bucket,
+                "Name": image,
+            }
+        },
+    )
+    return response
 
 
-detect_labels("bucket-test", "image-test")
+result = detect_labels("cvdemosept", "license-ca.jpeg")
+print(result)
